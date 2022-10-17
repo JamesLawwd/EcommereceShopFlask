f32d57c2c859aba0cdebcc6ceed5e21af652a070
Your NameJamesLawwd <you@example.comjleo56291@gmail.com>
2022-10-17 12:06:49 +0300

    First commit on EcommerceShop

---
+++ app.py
@@ -0,0 +1,48 @@
+# Client-server
+# Framework
+# Flask
+# Request-response
+
+# client -> listen to server response,send response to server (webbroweser)
+# sever-> process client request , sends response to the client (flask application)
+
+# Framework ,make development easy, by utilizing reusable code .... , flask, django
+
+from flask import Flask, render_template, sessions, request
+
+# start
+app = Flask(__name__)
+
+
+# decorators
+# flask routing
+# method -> route()
+# tto bind the method route to our app object, then we use decorator @
+@app.route('/home')
+def home_page():
+    return "Hello welcome to Flask"
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/')
+def index():
+    return "View index page here"
+
+
+@app.route('/add')
+def add():
+    cars = ['Mercedes', 'Subaru']
+    return cars
+
+
+app.run(debug=True)
+# End
